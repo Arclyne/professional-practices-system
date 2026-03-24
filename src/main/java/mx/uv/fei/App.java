54b87475dc652b617d7c7492c9b71179c9a0e8fa
angel gabriel aguilar hernandez <angelgabrielaguilarhernandez@angels-MacBook-Air.local>
2026-03-24 13:25:09 -0600

    Initial commit: nueva estructura con Java 25 and JavaFX

---
+++ src/main/java/mx/uv/fei/App.java
@@ -0,0 +1,33 @@
+package mx.uv.fei;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+
+        Label label = new Label("SPP - UV \nRunning on Java: " + javaVersion + "\nJavaFX: " + javafxVersion);
+        
+        String welcomeMessage = """
+                Welcome to the Professional Practices System.
+                Faculty of Statistics and Informatics (FEI).
+                """;
+        System.out.println(welcomeMessage);
+
+        Scene scene = new Scene(new StackPane(label), 400, 200);
+        stage.setTitle("Java 25 Environment Test");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
