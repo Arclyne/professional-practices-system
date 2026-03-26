270ae25338eb69d931e40ffd82a1b08ad98e3efa
angel gabriel aguilar hernandez <angelgabrielaguilarhernandez@angels-MacBook-Air.local>
2026-03-26 15:03:10 -0600

    database singleton con conexión dinamica y credenciales configurables.

---
+++ src/main/java/mx/uv/fei/config/FileConfigLoader.java
@@ -0,0 +1,25 @@
+package mx.uv.fei.config;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+public class FileConfigLoader {
+    public static Properties loadProperties(String fileName) {
+        Properties properties = new Properties();
+
+        try (InputStream input = FileConfigLoader.class.getClassLoader().getResourceAsStream(fileName)) {
+            if (input == null) {
+                throw new IllegalArgumentException("The configuration file was not found: " + fileName);
+            }
+            
+            properties.load(input);
+        } catch (IOException ex) {
+            throw new RuntimeException("Critical error reading the file: " + fileName, ex);
+        }
+
+        return properties;
+    }
+}
--- src/main/java/mx/uv/fei/dataacces/database/DatabaseConnection.java
+++ src/main/java/mx/uv/fei/dataacces/database/DatabaseConnection.java
@@ -1,29 +1,36 @@
 package mx.uv.fei.dataacces.database;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+import mx.uv.fei.config.FileConfigLoader;
+
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/registro_visita_fei";
-    private static final String USER = "spp_app_user";
-    private static final String PASSWORD = "User@2026";
+    private String url;
+    private String user;
+    private String password;
 
-    private Connection connection;
+    private static DatabaseConnection instance;
 
-    public DatabaseConnection() {
+    private DatabaseConnection() {
+        Properties databaseProperties = FileConfigLoader.loadProperties("database.properties");
+    
+        this.url = databaseProperties.getProperty("db.url");
+        this.user = databaseProperties.getProperty("db.user");
+        this.password = databaseProperties.getProperty("db.password");
+    }
+
+    public static DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
     }
 
     public Connection getConnection() throws SQLException {
-        if (connection == null || connection.isClosed()) {
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-        }
-        return connection;
-    }
-
-    public void closeConnection() throws SQLException {
-        if (connection != null && !connection.isClosed()) {
-            connection.close();
-        }
+        return DriverManager.getConnection(url, user, password);
     }
 }
