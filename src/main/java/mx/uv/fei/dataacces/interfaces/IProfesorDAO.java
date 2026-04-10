5cd296ac9626330933cf2266e9a071db63a2aca8
angel gabriel aguilar hernandez <angelgabrielaguilarhernandez@angels-MacBook-Air.local>
2026-04-10 15:48:36 -0600

    ProfessorDAO y PractitionerDAO Changed

--- src/main/java/mx/uv/fei/dataacces/interfaces/IProfesorDAO.java
+++
@@ -1,8 +0,0 @@
-package mx.uv.fei.dataacces.interfaces;
-
-import mx.uv.fei.dataacces.exceptions.DAOException;
-import mx.uv.fei.domain.dto.Profesor;
-
-public interface IProfesorDAO {
-    int insertProfesor(Profesor profesor) throws DAOException;
-}
