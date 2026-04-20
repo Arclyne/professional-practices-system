36a9518b99309ebae0bacf5c43d9667ee3ec78c1
poijn <josepriorhernandez@gmail.com>
2026-04-20 16:17:02 -0600

    spring droppet

---
+++ src/test/java/mx/uv/fei/config/FileConfigLoaderTest.java
@@ -0,0 +1,15 @@
+package mx.uv.fei.config;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Properties;
+import org.junit.jupiter.api.Test;
+
+public class FileConfigLoaderTest {
+    @Test
+    void testLoadProperties() {
+
+        Properties baseProperties = FileConfigLoader.loadProperties("database.properties");
+        assertNotNull(baseProperties);
+
+    }
+}
