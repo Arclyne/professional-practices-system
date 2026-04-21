908248963a61a513b502a4e9932a612f1f5bc995
poijn <josepriorhernandez@gmail.com>
2026-04-20 18:41:06 -0600

    Cambiar prefijo db por dataBase

--- src/test/java/mx/uv/fei/TestDatabaseSetup.java
+++ src/test/java/mx/uv/fei/TestDatabaseSetup.java
@@ -9,9 +9,9 @@ import mx.uv.fei.dataacces.interfaces.IDatabaseConnection;
 public class TestDatabaseSetup {
     public static void initialize(IDatabaseConnection dbConnection) throws SQLException {
         try (Connection conn = dbConnection.getConnection();
-                Statement stmt = conn.createStatement()) {
-            stmt.execute("RUNSCRIPT FROM 'classpath:schema.sql'");
-            stmt.execute("RUNSCRIPT FROM 'classpath:data.sql'");
+                Statement statement = conn.createStatement()) {
+            statement.execute("RUNSCRIPT FROM 'classpath:schema.sql'");
+            statement.execute("RUNSCRIPT FROM 'classpath:data.sql'");
         }
     }
 }
