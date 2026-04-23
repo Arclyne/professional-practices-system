fca6704020b01e38bc519c33b20ecdb86b4fcb0c
poijn23 <josepriorhernandez@gmail.com>
2026-04-23 12:13:56 -0600

    Frame de articulo

--- src/main/java/mx/uv/fei/App.java
+++ src/main/java/mx/uv/fei/App.java
@@ -1,31 +1,31 @@
 package mx.uv.fei;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("activityForms.fxml"));
+        try {
+            Parent root = loader.load();
 
-        Label label = new Label("SPP - UV \nRunning on Java: " + javaVersion + "\nJavaFX: " + javafxVersion);
+            Scene scene = new Scene(root);
 
-        String welcomeMessage = """
-                Welcome to the Professional Practices System.
-                Faculty of Statistics and Informatics (FEI).
-                """;
-        System.out.println(welcomeMessage);
+            stage.setTitle("Mi Primera Ventana con Scene Builder");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
 
-        Scene scene = new Scene(new StackPane(label), 400, 200);
-        stage.setTitle("Java Environment Test"); 
-        stage.setScene(scene);
-        stage.show();
+        }
     }
 
     @Override
