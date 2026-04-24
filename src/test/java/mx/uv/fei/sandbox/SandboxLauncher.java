0bc453ca107486f98c941c5504d63f96cfd45b9a
Arclyne <slaugtherboredom1@gmail.com>
2026-04-24 16:52:27 -0600

    FormField and styles

--- src/test/java/mx/uv/fei/sandbox/SandboxLauncher.java
+++ src/test/java/mx/uv/fei/sandbox/SandboxLauncher.java
@@ -2,7 +2,6 @@ package mx.uv.fei.sandbox;
 
 public class SandboxLauncher {
     public static void main(String[] args) {
-        GuiSandboxTwo.main(args);
-
+        GuiSandbox.main(args);
     }
 }
