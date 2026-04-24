cdd3133057a0242823394fad0661ff62b0c8b7f9
Arclyne <slaugtherboredom1@gmail.com>
2026-04-24 16:50:50 -0600

    FormField and styles

---
+++ src/main/java/mx/uv/fei/domain/manager/RegisterProfessorManager.java
@@ -0,0 +1,42 @@
+package mx.uv.fei.domain.manager;
+
+import java.util.UUID;
+import mx.uv.fei.domain.dto.Professor;
+import mx.uv.fei.dataacces.interfaces.IDatabaseConnection;
+import mx.uv.fei.dataacces.repositories.ProfessorDAO;
+import mx.uv.fei.dataacces.repositories.UserDAO;
+import mx.uv.fei.dataacces.exceptions.DAOException;
+import mx.uv.fei.domain..ManagerExeption;
+
+public class RegisterProfessorManager {
+
+    private final IDatabaseConnection dbConnection;
+
+    public RegisterProfessorManager(IDatabaseConnection dbConnection) {
+        this.dbConnection = dbConnection;
+    }
+
+    public String registerNewProfessor(Professor professor) throws ManagerExeption {
+        String tempPassword = "temp-" + UUID.randomUUID().toString().substring(0, 8);
+        professor.setPassword(tempPassword);
+
+        professor.setStatus("no activo");
+
+        try {
+            UserDAO userDAO = new UserDAO(dbConnection);
+            ProfessorDAO professorDAO = new ProfessorDAO(dbConnection, userDAO);
+
+            int resultId = professorDAO.insertProfessor(professor);
+
+            if (resultId <= 0) {
+                throw new ManagerExeption("No se pudo completar el registro del profesor en el sistema.");
+            }
+
+            return tempPassword;
+
+        } catch (DAOException e) {
+            e.printStackTrace();
+            throw new ManagerExeption("Ocurrió un problema de conexión con el servidor. Por favor, intente más tarde.", e);
+        }
+    }
+}
--- src/test/java/mx/uv/fei/sandbox/SandboxLauncher.java
+++ src/test/java/mx/uv/fei/sandbox/SandboxLauncher.java
@@ -2,7 +2,7 @@ package mx.uv.fei.sandbox;
 
 public class SandboxLauncher {
     public static void main(String[] args) {
-        GuiSandbox.main(args);
+        GuiSandboxTwo.main(args);
 
     }
 }
