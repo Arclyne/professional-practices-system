30d202b17b79f1eb2ae7dbd669ee2e67dbf0ed48
Arclyne <slaugtherboredom1@gmail.com>
2026-04-24 16:27:03 -0600

    sin terminar

--- src/test/java/mx/uv/fei/sandbox/GuiSandbox.java
+++ src/test/java/mx/uv/fei/sandbox/GuiSandbox.java
@@ -4,12 +4,29 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import mx.uv.fei.TestDatabaseSetup;
+import mx.uv.fei.config.DatabasePropeties;
+import mx.uv.fei.config.DataconnectionConfig;
+import mx.uv.fei.dataacces.interfaces.IDatabaseConnection;
+import mx.uv.fei.domain.manager.RegisterPractitionerManager;
+import mx.uv.fei.presentation.RegisterPractitionerController;
 
 public class GuiSandbox extends Application {
+
     @Override
     public void start(Stage stage) throws Exception {
+        DatabasePropeties propeties = new DatabasePropeties();
+        IDatabaseConnection dbConnection = new DataconnectionConfig(propeties, "test").databaseConnection();
+        TestDatabaseSetup.initialize(dbConnection);
+        RegisterPractitionerManager manager = new RegisterPractitionerManager(dbConnection);
+
+
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/mx/uv/fei/presentation/registerPractitioner.fxml"));
         Scene scene = new Scene(loader.load());
+
+        RegisterPractitionerController controller = loader.getController();
+        controller.setManager(manager);
+
         stage.setTitle("Sandbox - Vista de Registro");
         stage.setScene(scene);
         stage.show();
