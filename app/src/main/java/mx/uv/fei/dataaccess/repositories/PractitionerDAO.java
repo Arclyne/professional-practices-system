028dc16619c4c8fdda98de56938e3839f375bab1
Arclyne <slaugtherboredom1@gmail.com>
2026-06-05 00:45:43 -0600

    conflictos resueltos

--- app/src/main/java/mx/uv/fei/dataaccess/repositories/PractitionerDAO.java
+++ app/src/main/java/mx/uv/fei/dataaccess/repositories/PractitionerDAO.java
@@ -1,9 +1,6 @@
 package mx.uv.fei.dataaccess.repositories;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
